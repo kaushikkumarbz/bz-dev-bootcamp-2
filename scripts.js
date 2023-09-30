e4c4fbd7208f9247c899f2940f674993da624e20
kaushik kumar <kaushikk@beingzero.in>
2023-09-30 09:16:29 +0530

    added button to show alert message

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-console.log("Hello")
+    console.log("Hello")
+
+    // JavaScript code to display the alert
+
+    document.getElementById('alertButton').addEventListener('click', function() {
+        alert('Hello, this is your alert message!');
+    });
